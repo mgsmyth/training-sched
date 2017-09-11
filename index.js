d48026e15efe5f71ef638d6116ed574d9fd8980a
Meghan Gaudet <musicmocean@gmail.com>
2017-09-11 15:52:31 +0200

    use port in .env if there is one set

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
