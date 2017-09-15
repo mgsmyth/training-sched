f7e5548bd5b2c700e67dbefcf877597a076c5a34
Meghan Gaudet <musicmocean@gmail.com>
2017-09-15 23:51:36 +0200

    index.js: wire up express to handle new stylesheets + views dirs

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const routes = require('./routes');
 const app = express();
 
 const port = process.env.PORT || 8080;
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/css', express.static(__dirname + '/public/stylesheets'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use(routes);
 app.listen(port);
 
 console.log('Server running on port ' + port);
